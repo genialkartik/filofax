4e33036001a33cf34d63ce0d92385c5d2342ee15
Madan Lal <madanparmar637@gmail.com>
2021-03-16 21:58:01 +0530

    archieves upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './templates/Home';
 import Login from './templates/Login';
 import SignUp from './templates/SignUp';
+import Uploader from './templates/Uploader';
 
 function App(props) {
   return (
@@ -11,6 +12,7 @@ function App(props) {
         <Route component={Login} exact path="/login" />
         <Route component={SignUp} exact path="/register" />
         <Route component={Home} exact path="/" />
+        <Route component={Uploader} exact path="/archieves" />
       </Router>
     </div>
   );
