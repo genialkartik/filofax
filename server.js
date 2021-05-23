eb12abdd283b66bd8dfc6e435981c630d055fbe9
Kartik Tyagi <kartik9756@gmail.com>
2021-05-23 19:11:13 +0530

    build

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 
 // set PORT
-app.set("port", process.env.APP_PORT || 4000);
+app.set("port", process.env.APP_PORT || 8080);
 app.use(cors());
 app.use(express.json());
 
@@ -50,7 +50,7 @@ app.use("/pass", require("./route/creds"));
 app.use("/notes", require("./route/notebook"));
 app.use("/docs", require("./route/docs"));
 
-if (process.env.NODE_ENV == "Production") {
+if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
   app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
